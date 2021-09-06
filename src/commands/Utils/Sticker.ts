fe04acc7a8c98164b511723eac4ebd23cb1cc0d2
Nakshatra <87998363+Nakshatra4762@users.noreply.github.com>
2021-09-06 18:32:39 +0530

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -12,7 +12,7 @@ export default class Command extends BaseCommand {
             description: 'Converts images/videos into stickers',
             category: 'utils',
             usage: `${client.config.prefix}sticker [(as caption | tag)[video | image]]`,
-            dm: true,
+            dm: false,
             baseXp: 30
         })
     }
