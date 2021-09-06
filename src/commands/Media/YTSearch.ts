af21a72ded77874c832a4e90ddf8c57c1ac20e7c
Nakshatra <87998363+Nakshatra4762@users.noreply.github.com>
2021-09-06 17:51:08 +0530

    Update YTSearch.ts

--- src/commands/Media/YTSearch.ts
+++ src/commands/Media/YTSearch.ts
@@ -30,7 +30,7 @@ export default class Command extends BaseCommand {
                 videos[i].author.name
             }\n 📙 *Duration:* ${videos[i].duration}\n📘 *URL:* ${videos[i].url}\n\n`
         }
-        M.reply('👾 searching...')
+        M.reply('💫 searching...')
         this.client
             .sendMessage(M.from, text, MessageType.extendedText, {
                 quoted: M.WAMessage,
